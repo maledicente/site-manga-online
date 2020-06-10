bb7f5d94ea363389b0339d0e7e0040668d7f1f8c
maledicente <luizpaulonievola@ymail.com>
2020-06-10 09:48:23 -0300

    Primeiros arquivos

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Mangas X</title>
+   <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	
+	<header>
+		<?php
+			include "topo.php";
+		?>
+	</header>
+   
+</body>
+</html>
---
+++ topo.php
@@ -0,0 +1,8 @@
+<section id='logo_topo'>
+</section>
+<nav>
+	<a class='' href='index.php' target='_self'>HOME</a>
+	<a class='' href='index.php' target='_self'>Mangas</a>
+	<a class='' href='index.php' target='_self'>Ultimos lancamentos</a>
+	<a class='' href='index.php' target='_self'>Contato</a>
+</nav>
