b119fa0e191c7aaa650e61e6fc6366fc8b2c45f4
maledicente <luizpaulonievola@ymail.com>
2020-06-14 16:07:52 -0300

    Correcao display

--- principal.php
+++ principal.php
@@ -13,6 +13,10 @@
 		<section class="banner-mangas">	
 			<img class="imgbanner" src="imagens/banner/madara1.jpg" alt="confira">
 		</section>
+		
+	   <header id="header-mini">
+				<h2>Destaques</h2>
+		 </header>   
 
 		<section class="minimanga" >
 			<div class="miniatura">
@@ -62,19 +66,7 @@
 			<br>
 			<a href="#"><span >Chainsaw Man</a></span>
 			</div>
-
-			<div class="miniatura">
-			<a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
-			<br>
-			<a href="#"><span >Chainsaw Man</a></span>
-			</div>
-
-			<div class="miniatura">
-			<a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
-			<br>
-			<a href="#"><span >Chainsaw Man</a></span>
-			</div>
 		</section>
 
-
 	</section>
+
