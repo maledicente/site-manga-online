17ef691b10a62efa3bcc6aff3ba66bb0415584dd
maledicente <luizpaulonievola@ymail.com>
2020-06-16 13:39:12 -0300

    Display grid

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html lang="pt-br">
 <head>
    <meta charset="UTF-8">
@@ -7,26 +7,120 @@
    <link rel="stylesheet" href="css/main.css"/>
    <meta name="author" content="Luiz Paulo Nievola">
    <link rel="icon" href="imagens/icon.png">
+   <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 </head>
-<body>
+<body> 
 	
-	<header>
-		<?php
-			include "topo.php"; 
-		?>
-	</header> 
+	<div class="wrapper"> 
+		<!-- Navigation -->
+    <nav class="main-nav">
+      <div class="logo"><img src=imagens/logo.png alt="confira"></div>
 
-	<section id="conteiner-principal">
-		<?php
-			include "principal.php"; 
-		?>
-	</section>
+      <ul class="nav">
+        <li>
+          <a href="#">Home</a> 
+        </li>   
+        <li>
+          <a href="#">Mangas</a>
+        </li>
+        <li>
+          <a href="#">Lancamentos</a>
+        </li>
+        <li>
+          <a href="#">Contato</a>
+        </li>
+      </ul>
+        
+        <div class="busca">
+          <input type="text" placeholder="Buscar..."/>
+            <img src="imagens/search.png" id="btnBusca" alt="Buscar"/>
+            <img src="imagens/user.png" id="btnBusca" alt="user"/>
+        </div>
 
-	<footer id="rodape">
-		<?php
-			include "footer.php"; 
-		?>
+    </nav>
+		
+		<!-- Top Container -->
+    <section class="top-container">
+
+      <header class="banner">
+        <h1>Confira este manga</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, ipsam! Nihil quo minima nulla atque!</p>
+        <a href="#" class="btn">Leia mais</a>
+      </header>
+
+      <div class="top-box top-box-a">
+        <h4>Top 10 Mangas</h4>
+        <p class="price">$0.99/mes</p>
+        <a href="" class="btn">Compre agora</a>
+      </div>
+
+      <div class="top-box top-box-b">
+        <!--<h4>Mangas mais lidos</h4> -->
+          <div class="miniatura">
+           <a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
+             <br>
+           <a href="#"><span class="btn" >Chainsaw Man</a></span>
+          </div> 
+
+           <div class="miniatura">
+           <a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
+             <br>
+           <a href="#"><span class="btn" >Chainsaw Man</a></span>
+          </div>
+
+           <div class="miniatura">
+           <a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
+             <br>
+           <a href="#"><span class="btn" >Chainsaw Man</a></span>
+          </div>
+
+           <div class="miniatura">
+           <a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
+             <br>
+           <a href="#"><span class="btn" >Chainsaw Man</a></span>
+          </div> 
+
+           <div class="miniatura">
+           <a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
+             <br>
+           <a href="#"><span class="btn" >Chainsaw Man</a></span>
+          </div> 
+
+           <div class="miniatura">
+           <a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
+             <br>
+           <a href="#"><span class="btn" >Chainsaw Man</a></span>
+          </div> 
+
+           <div class="miniatura">
+           <a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
+             <br>
+           <a href="#"><span class="btn" >Chainsaw Man</a></span>
+          </div> 
+
+           <div class="miniatura">
+           <a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
+             <br>
+           <a href="#"><span class="btn" >Chainsaw Man</a></span>
+          </div> 
+
+           <div class="miniatura">
+           <a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
+             <br>
+           <a href="#"><span class="btn" >Chainsaw Man</a></span>
+          </div> 
+      </div>
+
+
+    </section>
+		
+
+	<footer class="footer">
+		<p>Copyright &copy; 2020 - by Luiz Paulo Nievola<br/>
+		 <a href="https://github.com/maledicente" target="_blank">GitHub</a></p>
 	</footer>
+	</div>
+  <!-- Wrapper Ends -->
    
 </body>
 </html>
--- principal.php
+++ principal.php
@@ -1,15 +1,13 @@
+  
 <section class="conteudo-principal">
 	   <aside class="top-manga">
 			<header>
 				<h1>TOP 10 MANGAS</h1>
-
 			<section class="minimanga" >
 			
 		</section>
-
 			</header>
 		</aside>
-
 		<section class="banner-mangas">	
 			<img class="imgbanner" src="imagens/banner/madara1.jpg" alt="confira">
 		</section>
@@ -17,56 +15,46 @@
 	   <header id="header-mini">
 				<h2>Destaques</h2>
 		 </header>   
-
 		<section class="minimanga" >
 			<div class="miniatura">
 			<a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
 			<br>
 			<a href="#"><span >Chainsaw Man</a></span>
 			</div>
-
 			<div class="miniatura">
 			<a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
 			<br>
 			<a href="#"><span >Chainsaw Man</a></span>
 			</div>
-
 			<div class="miniatura">
 			<a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
 			<br>
 			<a href="#"><span >Chainsaw Man</a></span>
 			</div>
-
 			<div class="miniatura">
 			<a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
 			<br>
 			<a href="#"><span >Chainsaw Man</a></span>
 			</div>
-
 			<div class="miniatura">
 			<a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
 			<br>
 			<a href="#"><span >Chainsaw Man</a></span>
 			</div>
-
 			<div class="miniatura">
 			<a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
 			<br>
 			<a href="#"><span >Chainsaw Man</a></span>
 			</div>
-
 			<div class="miniatura">
 			<a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
 			<br>
 			<a href="#"><span >Chainsaw Man</a></span>
 			</div>
-
 			<div class="miniatura">
 			<a href="#"><img src="imagens/5e11c803-ChainsawMan.jpg" alt=""></a>
 			<br>
 			<a href="#"><span >Chainsaw Man</a></span>
 			</div>
 		</section>
-
 	</section>
-
