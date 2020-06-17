4687e5a83834e004129d38a702eef0c394229777
maledicente <luizpaulonievola@ymail.com>
2020-06-17 13:23:26 -0300

    Banner animado

---
+++ js/main.js
@@ -0,0 +1,54 @@
+var imgs=[];
+var slider;
+var imgAtual;
+var maxImg;
+var tmp;
+var tempoTroca;
+var vtempo;
+
+function preLoad(){
+	var s=1;
+	
+	for(var i=0;i<5;i++){
+		imgs[i]= new Image();
+		imgs[i].src="imagens/banner/s"+s+".jpg";
+			s++;
+	}
+}
+
+function loadImg(img){
+	slider.style.backgroundImage = "url('"+imgs[img].src+"')";
+}
+
+function start(){
+	preLoad();
+	imgAtual=0;
+	maxImg=imgs.length-1;
+	slider=document.getElementsByClassName("banner")[0];
+	loadImg(imgAtual);
+	tempoTroca=0;
+	anima();
+}
+
+function troca(dir){
+	tempoTroca=0;
+	imgAtual+=dir; 
+	if(imgAtual > maxImg){
+		imgAtual=0;
+	}else if(imgAtual < 0){
+		imgAtual=maxImg;
+	}
+	loadImg(imgAtual);
+}
+
+function anima(){
+	tempoTroca++;
+	if(tempoTroca >= 500){
+		tempoTroca=0;
+		troca(1);
+	}
+	vtempo=tempoTroca/5;
+	window.requestAnimationFrame(anima);
+}
+
+window.addEventListener("load", start);
